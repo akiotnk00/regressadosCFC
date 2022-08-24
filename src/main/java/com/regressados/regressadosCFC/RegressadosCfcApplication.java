8dadd8cdf6ef5ec8f751c7fe4b4f86c486f8b2c6
akiotnk00 <akiotnk00@gmail.com>
2022-08-24 14:26:51 -0300

    Inicio do Projeto

---
+++ src/main/java/com/regressados/regressadosCFC/RegressadosCfcApplication.java
@@ -0,0 +1,13 @@
+package com.regressados.regressadosCFC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegressadosCfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegressadosCfcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/regressados/regressadosCFC/RegressadosCfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regressados.regressadosCFC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegressadosCfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
